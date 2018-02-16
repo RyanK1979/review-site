1ba08d409ee49a2a092f0f8284ac98a1ca0db3a7
RyanK79 <Nermal5@msn.com>
2018-02-16 13:52:18 -0500

    Finished the testing phase, and controller

--- src/main/java/org/wecancodeit/reviewsSite/ReviewsController.java
+++ src/main/java/org/wecancodeit/reviewsSite/ReviewsController.java
@@ -1,13 +1,28 @@
 package org.wecancodeit.reviewsSite;
 
-import javax.annotation.Resources;
+import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReviewsController {
 
-	@Resources
-	ReviewsRepository repository = new ReviewsRepository();
+	@Resource
+	ReviewsRepository reviewsRepo = new ReviewsRepository();
+
+	@RequestMapping(value = "reviews")
+	public String getAllCourses(Model model) {
+		model.addAttribute("reviews", reviewsRepo.findAll());
+		return "reviews";
+	}
+
+	@RequestMapping("review")
+	public String getACourse(@RequestParam Long id, Model model) {
+		model.addAttribute("reviews", reviewsRepo.findOne(id));
+		return "review";
+	}
 
 }
--- src/main/java/org/wecancodeit/reviewsSite/ReviewsRepository.java
+++ src/main/java/org/wecancodeit/reviewsSite/ReviewsRepository.java
@@ -12,14 +12,24 @@ public class ReviewsRepository {
 	Map<Long, Review> reviews = new HashMap<>();
 
 	public ReviewsRepository() {
-		Review Fortnight = new Review(1L, "Fortnight", "Fortnight Description");
-		Review PubG = new Review(2L, "PubG", "PubG Description");
-		Review Battlefield = new Review(3L, "Battlefield 1", "Battlefield1 Description");
+		Review Fortnight = new Review(21L, "Fortnight", "Fortnight Description");
+		Review PubG = new Review(22L, "PubG", "PubG Description");
+		Review Battlefield = new Review(23L, "Battlefield 1", "Battlefield1 Description");
 
 		reviews.put(Fortnight.getId(), Fortnight);
 		reviews.put(PubG.getId(), PubG);
 		reviews.put(Battlefield.getId(), Battlefield);
 
+	}
+
+	public ReviewsRepository(Review... reviews) {
+		populateReviewsMap(reviews);
+	}
+
+	private void populateReviewsMap(Review... reviews) {
+		for (Review review : reviews) {
+			this.reviews.put(review.getId(), review);
+		}
 	}
 
 	public Review findOne(Long reviewId) {
